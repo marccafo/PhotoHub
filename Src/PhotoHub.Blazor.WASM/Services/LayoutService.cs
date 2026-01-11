382abfaa749fb8225319a9c315f70be5f12a3a0d
Marc Caralps Fontrubí <marc.caralps.fontrubi@gmail.com>
2026-01-11 23:03:34 +0100

    feat: Add dark mode

--- Src/PhotoHub.Blazor.WASM/Services/LayoutService.cs
+++ Src/PhotoHub.Blazor.WASM/Services/LayoutService.cs
@@ -1,10 +1,48 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace PhotoHub.Blazor.WASM.Services;
 
 public class LayoutService
 {
+    private readonly IJSRuntime _jsRuntime;
+    private const string DarkModeKey = "isDarkMode";
+
+    public LayoutService(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
     public event Action? OnMajorUpdate;
+
+    private bool _isDarkMode = true;
+    public bool IsDarkMode
+    {
+        get => _isDarkMode;
+        private set
+        {
+            if (_isDarkMode != value)
+            {
+                _isDarkMode = value;
+                NotifyUpdate();
+            }
+        }
+    }
+
+    public async Task InitializeAsync()
+    {
+        var storedValue = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", DarkModeKey);
+        if (bool.TryParse(storedValue, out var isDark))
+        {
+            IsDarkMode = isDark;
+        }
+    }
+
+    public async Task ToggleDarkModeAsync()
+    {
+        IsDarkMode = !IsDarkMode;
+        await _jsRuntime.InvokeVoidAsync("localStorage.setItem", DarkModeKey, IsDarkMode.ToString().ToLower());
+    }
 
     private bool _isNavbarCustom;
     public bool IsNavbarCustom
